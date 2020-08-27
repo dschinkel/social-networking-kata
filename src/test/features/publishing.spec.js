4d34a50ad738f981502a405fcaa4d722370bf807
Dave Schinkel <dschinkel@gmail.com>
2020-08-27 15:46:26 -0500

    Refactor: Cleanup tests.  Not much to cleanup in prod code yet.

--- src/test/features/publishing.spec.js
+++ src/test/features/publishing.spec.js
@@ -4,19 +4,21 @@ import Timeline from '../../Timeline';
 describe('Feature: Publishing', () => {
 
 	describe('Scenario: Alice publishes messages to her personal timeline', () => {
-		const alice = User("Alice");
+		const name = "Alice"
+		const message = "I love the weather today.";
+		const alice = User(name);
 		const aliceTimeline = Timeline(alice);
 
 		describe('Given Alice has published "I love the weather today."', () => {
-			aliceTimeline.publish('I love the weather today.');
+			aliceTimeline.publish(message);
 		});
 
 		describe('When Alice views her timeline', () => {
 			const timelineMessage = aliceTimeline.view();
 
 			it('Then Alice sees: "I love the weather today."', () => {
-				expect(alice.getName()).to.equal("Alice");
-				expect(timelineMessage).to.equal("I love the weather today.");
+				expect(alice.getName()).to.equal(name);
+				expect(timelineMessage).to.equal(message);
 			});
 		});
 	});
